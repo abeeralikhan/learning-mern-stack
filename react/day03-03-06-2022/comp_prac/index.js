ecf8d3b68aa1742191ba1799c61033d1d2121bc4
Abeer Ali Khan <aalkhan@students.uit.edu>
2022-06-03 20:59:59 +0500

    react: day 03 v1

--- react/day03-03-06-2022/comp_prac/index.js
+++ react/day03-03-06-2022/comp_prac/index.js
@@ -15,6 +15,29 @@ class DateNow extends React.Component {
         return <h1>Date: {Date.now()}</h1>
     }
 }
+
+function getNum() {
+    return Math.floor((Math.random() * 10) + 1);
+}
+class NumPicker extends React.Component {
+    render() {
+        const number = getNum();
+        let message;
+        if (number === 4) {
+            message = <p>Yippieee</p>
+        } else {
+            message = <p>Alas!</p>
+        }
+
+        return (
+            <div>
+                <h1>Your number is: {number}</h1>
+                <h2>{ number === 4 ? "Congrats!" : "Try Again!" }</h2>
+                <h2>{ message }</h2>
+            </div>
+        );
+    }
+}
 // ReactDOM.render(<TellYourName />, document.getElementById('root'));
 // ReactDOM.render(<OutYourName />, document.getElementById('root'));
-ReactDOM.render(<DateNow />, document.getElementById('root'));
+ReactDOM.render(<NumPicker />, document.getElementById('root'));
