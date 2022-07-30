0cbb3958441d19d653f92fe5cb2d89a54997b37c
Abeer Ali Khan <aalkhan@students.uit.edu>
2022-07-30 17:55:34 +0500

    react: day09 update

--- react/day09-30-07-2022/more-states-fun/src/RollDice.js
+++ react/day09-30-07-2022/more-states-fun/src/RollDice.js
@@ -3,12 +3,31 @@ import { Component } from "react";
 import Die from "./Die";
 
 class RollDice extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            dice1: 1,
+            dice2: 1
+        };
+        this.roll = this.roll.bind(this);
+    }
+    getRandomSide() {
+        return Math.floor((Math.random() * 6) + 1);
+    }
+    roll() {
+        this.setState({
+            dice1: this.getRandomSide(),
+            dice2: this.getRandomSide(),
+        });
+    }
     render() {
+        // this.roll();
         return (
-            <>
-                <Die diceNumber={5}/>
-                <Die diceNumber={3}/>
-            </>
+            <div>
+                <Die diceNumber={this.state.dice1}/>
+                <Die diceNumber={this.state.dice2}/>
+                <button className="RollDice-button" onClick={this.roll}>Roll Dice!</button>
+            </div>
         );
     }
 }
