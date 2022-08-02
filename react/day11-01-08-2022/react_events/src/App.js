c27c17d45c855a419dab75a0439ffb832eb26706
Abeer Ali Khan <aalkhan@students.uit.edu>
2022-08-03 04:22:32 +0500

    react: day 11

---
+++ react/day11-01-08-2022/react_events/src/App.js
@@ -0,0 +1,16 @@
+import { Component } from 'react';
+// import AnnoyingForm from './AnnoyingForm';
+// import RandomQuotes from './RandomQuotes';
+import ButtonList from './ButtonList';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ButtonList />
+      </div>
+    )
+  }
+}
+
+export default App;
