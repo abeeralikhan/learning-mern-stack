ffd3ab3f22ba2d52b996d16d002ea2a9886b2e95
Abeer Ali Khan <aalkhan@students.uit.edu>
2022-08-23 19:37:14 +0500

    react: day 22 work

--- react/day22-23-08-2022/react-router-patterns/src/FoodSearch.js
+++ react/day22-23-08-2022/react-router-patterns/src/FoodSearch.js
@@ -1,43 +1,44 @@
-import { Component } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Component } from "react";
+import { Link } from "react-router-dom";
 
 class FoodSearch extends Component {
-	constructor(props) {
-		super(props);
-		this.state = { query: '' };
-		this.handleChange = this.handleChange.bind(this);
-		this.handleClick = this.handleClick.bind(this);
-	}
+  constructor(props) {
+    super(props);
+    this.state = { query: "" };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-	handleChange(e) {
-		this.setState({ [e.target.name]: e.target.value });
-	}
+  handleChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
 
-	handleClick() {
-		// Do some operations
-		alert('Your data has been saved!');
+  handleClick() {
+    // Do some operations
+    alert("Your data has been saved!");
 
-		// redirect user
-		this.props.history.push('/');
-	}
+    // redirect user
+    // const navigate = useNavigate();
+    // navigate(`/food/${this.state.query}`);
+  }
 
-	render() {
-		return (
-			<div>
-				<Link to="/">Back to home</Link>
-				<h1>Search For A Food!</h1>
-				<input
-					type="text"
-					name="query"
-					placeholder="Search food"
-					value={this.state.query}
-					onChange={this.handleChange}
-				/>
-				<Link to={`/food/${this.state.query}`}> Go </Link>
-				<button onClick={this.handleClick}>Save food data!</button>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <Link to="/">Back to home</Link>
+        <h1>Search For A Food!</h1>
+        <input
+          type="text"
+          name="query"
+          placeholder="Search food"
+          value={this.state.query}
+          onChange={this.handleChange}
+        />
+        <Link to={`/food/${this.state.query}`}> Go </Link>
+        <button onClick={this.handleClick}>Save food data!</button>
+      </div>
+    );
+  }
 }
 
 export default FoodSearch;
