793b8cade3e08f15f0dd7173e4d2cf8e90ce954f
Abeer Ali Khan <aalkhan@students.uit.edu>
2022-09-25 02:56:13 +0500

    node: authentication & security day 01

---
+++ node_js/security-and-authentication/security-example/index.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const path = require("path");
+const https = require("https");
+const express = require("express");
+
+const app = express();
+
+const PORT = 3000;
+
+app.get("/secret", (req, res) => {
+  return res.send("Your secret code is 963!");
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+https
+  .createServer(
+    {
+      key: fs.readFileSync("key.pem"),
+      cert: fs.readFileSync("cert.pem"),
+    },
+    app
+  )
+  .listen(PORT, () => {
+    console.log(`Server is listening at ${PORT}`);
+  });
