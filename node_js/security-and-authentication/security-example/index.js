e5392989cedf4f61c552a88f00df46188d079ec7
Abeer Ali Khan <aalkhan@students.uit.edu>
2022-09-26 01:33:09 +0500

    node: authentication & security day 02

--- node_js/security-and-authentication/security-example/index.js
+++ node_js/security-and-authentication/security-example/index.js
@@ -1,11 +1,14 @@
 const fs = require("fs");
 const path = require("path");
 const https = require("https");
+const helmet = require("helmet");
 const express = require("express");
 
 const app = express();
 
 const PORT = 3000;
+
+app.use(helmet());
 
 app.get("/secret", (req, res) => {
   return res.send("Your secret code is 963!");
