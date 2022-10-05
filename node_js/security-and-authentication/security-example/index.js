4a33a9c681abbb2ae16cc6bce88c41bc4b6001aa
Abeer Ali Khan <aalkhan@students.uit.edu>
2022-10-05 17:13:09 +0500

    node: authentication & security day 04

--- node_js/security-and-authentication/security-example/index.js
+++ node_js/security-and-authentication/security-example/index.js
@@ -3,14 +3,52 @@ const path = require("path");
 const https = require("https");
 const helmet = require("helmet");
 const express = require("express");
+const passport = require("passport");
+const { Strategy } = require("passport-google-oauth20");
 
-const app = express();
+require("dotenv").config();
 
 const PORT = 3000;
 
-app.use(helmet());
+const config = {
+  CLIENT_ID: process.env.CLIENT_ID,
+  CLIENT_SECRET: process.env.CLIENT_SECRET,
+};
 
-app.get("/secret", (req, res) => {
+const AUTH_OPTIONS = {
+  callbackURL: "/auth/google/callback",
+  clientID: config.CLIENT_ID,
+  clientSecret: config.CLIENT_SECRET,
+};
+
+function verifyCallback(accessToken, refreshToken, profile, done) {
+  console.log("Google profile", profile);
+  done(null, profile);
+}
+
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
+
+const app = express();
+
+app.use(helmet());
+app.use(passport.initialize());
+
+function checkLoggedIn(req, res, next) {
+  // let isLoggedIn = false;
+  let isLoggedIn = true;
+
+  if (!isLoggedIn) {
+    return res.status(401).json({ error: "You must log in!" });
+  }
+
+  next();
+}
+
+app.get("/auth/google", (req, res) => {});
+app.get("/auth/google/callback", (req, res) => {});
+app.get("/auth/logout", (req, res) => {});
+
+app.get("/secret", checkLoggedIn, (req, res) => {
   return res.send("Your secret code is 963!");
 });
 
